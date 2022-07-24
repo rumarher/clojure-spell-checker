3e12107ff0fd45cba3a27781606eaeadc1c19925
Raúl Ulises Martín Hernández <martin.jurses@gmail.com>
2022-07-24 23:44:06 +0100

    Primera parte

--- src/clojure_spellchecker/core.clj
+++ src/clojure_spellchecker/core.clj
@@ -1,7 +1,16 @@
 (ns clojure-spellchecker.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(def words
+    (set (map str/trim (str/split-lines (slurp "resources/wordsEn.txt")))))
+
+
+(defn correct? [word]
+  (contains? words word))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+)
