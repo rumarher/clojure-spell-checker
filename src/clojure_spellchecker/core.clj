e036316559bb643cac4089e8b1f5f58da4e36b1c
Raúl Ulises Martín Hernández <martin.jurses@gmail.com>
2022-07-26 22:34:46 +0100

    Terminado ejercicio
    Ahora lee cadenas desde la ejecución del programa principal. En el
    caso de que la palabra no esté escrita correctamente dirá que no es
    correcta y dará como solución la próxima con la distancia más próxima
    entre ellas.

--- src/clojure_spellchecker/core.clj
+++ src/clojure_spellchecker/core.clj
@@ -1,16 +1,28 @@
 (ns clojure-spellchecker.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import (org.apache.commons.lang3 StringUtils)))
 
 (def words
     (set (map str/trim (str/split-lines (slurp "resources/wordsEn.txt")))))
 
+(defn distance [word1 word2]
+  (StringUtils/getLevenshteinDistance word1 word2))
+
+(defn min-distance [word]
+  (apply min-key (partial distance word) words))
 
 (defn correct? [word]
   (contains? words word))
 
+(defn askIfCorrect [word]
+  (if (correct? word)
+    (println "The word: " word " is well written.")
+    (println word " incorrect. Did you mean?" (min-distance word))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (let [word (first args)] 
+    (askIfCorrect word))
 )
